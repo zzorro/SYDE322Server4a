9fad738040a4e54ee099b3cc9bd40b9f0bf1c214
Andy <zzorro444@gmail.com>
2012-03-30 02:07:26 -0400

    update seed.db with a sample song

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+   Song.create( name: 'rolling in the deep', artist: 'Adele', lyrics: "There's a fire starting in my heart
+Reaching a fever pitch and it's bring me out the dark
+Finally I can see you crystal clear
+Go ahead and sell me out and I'll lay your ship bare
+
+See how I'll leave, with every piece of you
+Don't underestimate the things that I will do
+There's a fire starting in my heart
+Reaching a fever pitch and it's bring me out the dark
+
+The scars of your love remind me of us
+They keep me thinking that we almost had it all
+The scars of your love, they leave me breathless
+I can't help feeling
+
+We could have had it all
+Rolling in the deep
+You had my heart inside your hand
+And you played it to the beat
+
+Baby, I have no story to be told
+But I've heard one of you and I'm gonna make your head burn
+Think of me in the depths of your despair
+Making a home down there as mine sure won't be shared
+
+The scars of your love remind you of us
+They keep me thinking that we almost had it all
+The scars of your love, they leave me breathless
+I can't help feeling
+
+We could have had it all
+Rolling in the deep
+You had my heart inside your hand
+And you played it to the beat
+Could have had it all
+Rolling in the deep
+You had my heart inside your hand
+But you played it with a beating
+
+Throw your soul through every open door
+Count your blessings to find what you look for
+Turn my sorrow into treasured gold
+You pay me back in kind and reap just what you sow
+
+(Now I'm gonna wish you never had met me)
+We could have had it all
+(Tears are gonna fall, rolling in the deep)
+We could have had it all
+
+(Now I'm gonna wish you never had met me)
+It all, it all, it all, it all
+(Tears are gonna fall, rolling in the deep)
+
+(Now I'm gonna wish you never had met me)
+We could have had it all
+(Tears are gonna fall, rolling in the deep)
+Rolling in the deep
+
+(Now I'm gonna wish you never had met me)
+You had my heart and soul
+(Tears are gonna fall, rolling in the deep)
+And you played it to the beat
+
+(Now I'm gonna wish you never had met me)
+Could have had it all
+(Tears are gonna fall, rolling in the deep)
+Rolling in the deep
+
+(Now I'm gonna wish you never had met me)
+You had my heart and soul in your hand
+(Tears are gonna fall, rolling in the deep)
+But you played it, you played it, you played it to the beat"
+)
